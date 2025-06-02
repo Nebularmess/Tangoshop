b74a17ce3c7129c3f6e8eeeb8b8a7d502fa60b10
SIX <simon.494.perrez@gmail.com>
2025-06-02 12:40:15 -0300

    cko

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import * as React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 interface ProductCardProps {
